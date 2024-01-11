d75ce89c1dcafd04ed80a420d6a68f500899e947
Ali Amaan <s.aliamaan.official@gmail.com>
2024-01-11 13:01:33 +0500

    update sensor data

--- services/device.service.js
+++ services/device.service.js
@@ -1,6 +1,18 @@
+import ApiError from "../errors/ApiError.js";
 import Device from "../models/Device.js"
 
 export const storeData= async (deviceData)=>{
+    const getData = await Device.findOne({
+        deviceID:deviceData.deviceID
+    })
+    if(getData)
+    {
+        const updateData = await Device.findOneAndUpdate(
+            {deviceID: deviceData.deviceID},{soildata: deviceData.data},{new:true}
+
+        )
+        return updateData;
+    }
     const newData = await new Device(
         {
             deviceID:deviceData.deviceID,
